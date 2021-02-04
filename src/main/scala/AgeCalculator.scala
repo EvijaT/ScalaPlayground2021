00803daf65dcfdb2c95136a1121b194d61bd882c
Evija <evija.tilaka@gmail.com>
2021-02-04 20:39:54 +0200

    Age calculator task - done

--- src/main/scala/AgeCalculator.scala
+++ src/main/scala/AgeCalculator.scala
@@ -1,7 +1,15 @@
+import scala.io.StdIn.readLine
+
 object AgeCalculator extends App {
   println("This program will calculate the year when you will be 100")
   //TODO ask for person's name
   //TODO ask for person's age
   //TODO calculate and display year when they will be 100 :)
+
+  val myName = readLine("What is your name?")
+  val myAge = readLine(s"Nice to meet you, $myName! How old are you?").toFloat
+  val currentYear = readLine (s"Nice! What is the current year, $myName?").toFloat
+  val whenHundred = currentYear +100 - myAge
+  println(s"Random fact, $myName! The year you will turn 100 years old is ${whenHundred.round}!")
 
 }
